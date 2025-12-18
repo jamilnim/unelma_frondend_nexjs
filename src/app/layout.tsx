1b3694fd9a520084f968ae9f814c1fbfb36ac804
Mahabub-Hasan-Bayzid <s2500143@edu.bc.fi>
2025-12-18 15:35:26 +0200

    Add scroll to top button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "../component/header/Header";
 import Footer from "../component/footer/Footer";
 import Providers from "./Providers";
 import ThemeProvider from "../component/Theme/ThemeProvider";
+import ScrollToTop from "../component/ScrollToTop/ScrollToTop";
 
 export const metadata = {
   title: "Case Study App",
@@ -15,10 +16,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <StoreProvider>
-        <ThemeProvider>
-          <Header />
-          <Providers>{children}</Providers>
-          <Footer />
+          <ThemeProvider>
+            <Header />
+            <Providers>{children}</Providers>
+            <Footer />
+            <ScrollToTop />
           </ThemeProvider>
         </StoreProvider>
       </body>
